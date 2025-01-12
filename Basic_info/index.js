287b78831b4b6c5ee0a6b13419f9b87ac67e67a5
Dhirendra Prajapati <87705336+Gajendra-bhai@users.noreply.github.com>
2025-01-12 10:59:04 +0000

    variable declaration

---
+++ Basic_info/index.js
@@ -0,0 +1,6 @@
+//Variable Declaration
+let accountName = "Dhirendra@gmail.com"  //  used to declare local variables that are scoped to blocks: 
+var accountCity = "Mumbai"; // used to declared global variable that are not scoped to blocked
+const password = "abc@123" //used to declare a constant variable, which means that the variable's value cannot be reassigned
+
+console.table([accountName,accountCity,password])
